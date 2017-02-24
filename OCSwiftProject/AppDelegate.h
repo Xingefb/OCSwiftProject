3c0489142043cef6f5796fe0d576ee3e46bee3cc
Xingefb <dzy920210@gmail.com>
2017-02-24 16:14:04 +0800

    init

---
+++ OCSwiftProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCSwiftProject
+//
+//  Created by Dzy on 24/02/2017.
+//  Copyright © 2017 Dzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCSwiftProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCSwiftProject
+//
+//  Created by Dzy on 24/02/2017.
+//  Copyright © 2017 Dzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
